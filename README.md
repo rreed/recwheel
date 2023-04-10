68dbf5a57edbbc779d3511946a8dbac09d4dce5b
Robin Reed <robin.reed@sonatus.com>
2023-04-09 19:32:29 -0700

    Initial upload I guess

---
+++ README.md
@@ -0,0 +1,75 @@
+### Recwheel
+
+A small Sunday afternoon scale Discord bot, implementing a "recommendation wheel" based on a random conversation with one of my metamours.
+
+This should be considered a work in progress, but it works well enough for a small server. :)
+
+### The Impetus
+
+```
+[redacted]:
+I know what they mean, but the way I immediately interpreted this as “I would participate in the polycule discord”
+
+Amdusias:
+hahaha I mean we joke about how we already have a video chat service and a graphing tool, we may as well
+
+[redacted]:
+This would be an album recommendation roulette wheel thing
+
+Amdusias:
+yeah, that's a neat idea
+
+[redacted]:
+Currently a bot on telegram in a music chat I’m in
+
+Amdusias:
+so how does this work?
+if your slot comes up you have an album ready to go?
+
+[redacted]:
+When the wheel is spun it pairs you with a random other user on the wheel
+And you ask them what kind of album they’re feeling, gage their taste
+Snaking, not pairs
+
+Amdusias:
+ah cool!
+
+[redacted]:
+It’s a cool way to share music
+
+Amdusias:
+yeah! that's actually cool for sure
+sounds like something [redacted] would like too
+
+[redacted]:
+Could easily work with any other medium too (film, books, etc)
+
+Amdusias:
+and it's also like
+god I do *not* need another project hahaha
+but writing the discord bot to do that is actually easy
+```
+
+### Participation
+
+Users may have one of four participation levels:
+- `ALWAYS`: they will be included in all future spins of the wheel
+- `ONCE`: they will be included in the next spin of the wheel
+- `INTERESTED`: they will be notified before the wheel is spun, but not actually included in the spin itself unless they re-tag to `ALWAYS` or `ONCE`
+- `NEVER`: not a real level, as this is just "not actually in the user database"; `remove me` deletes oneself from the database because that makes the API simpler ;)
+
+### Using It
+
+First, install it as a Discord bot. The exact steps to doing this are outside the scope of this README, but [here](https://discord.com/developers/docs/getting-started) is Discord's documentation for doing so at time of writing.
+
+The API, once it's installed, is as follows (all requests must be directed to the bot with `@recwheel`, or whatever else you named it):
+- `add me once`: sign up for participating the next time the wheel is spun
+- `add me always`: sign up for participating in all future spins
+- `remove me`: no longer particpate in future wheel spins
+- `prespin`: notifies users that the wheel is about to be spun, tags all users registered as "interested"
+- `spin`: spins the wheel
+- `help`: prints this menu
+
+When `prespin` is run, all participants at all levels are notified. This gives users a chance to opt in before the wheel is spun.
+
+When `spin` is run, all users tagged `ALWAYS` or `ONCE` are put into a list and then shuffled. Each user in this list is pointed to the next user, and the last pointed to the first. Each user should then discuss the chosen topic with the user that their arrow points to.
---
+++ recbot.py
@@ -0,0 +1,146 @@
+import discord
+import os
+import sqlite3
+import random
+
+from dotenv import load_dotenv
+from enum import IntEnum
+
+DB_NAME = "recs.db"
+
+HELP_TEXT = """
+Hi! Please choose from one of the following:
+
+- `add me once`: sign up for participating the next time the wheel is spun
+- `add me always`: sign up for participating in all future spins
+- `remove me`: no longer particpate in future wheel spins
+- `prespin`: notifies users that the wheel is about to be spun, tags all users registered as "interested"
+- `spin`: spins the wheel
+- `help`: prints this menu
+"""
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+class ParticipationLevel(IntEnum):
+    # we don't actually need a `never` because `remove me` just deletes you from the database
+    ONCE = 1
+    ALWAYS = 2
+    INTERESTED = 3
+
+@client.event
+async def on_ready():
+    print(f"Authenticated as {client.user}")
+
+@client.event
+async def on_message(message):
+    # some no-ops first, let's try to avoid infinite recursions here :)
+    # don't bother logging in either case because they're never really meaningful logs
+    if message.author == client.user:
+        return
+
+    if not client.user.mentioned_in(message):
+        return
+
+    bot_id = client.user.id
+    actual_content = message.content.replace(f"<@{bot_id}>", "").strip()
+    author_id = message.author.id
+
+    conn = sqlite3.connect(DB_NAME)
+    match actual_content:
+        case "help":
+            await message.channel.send(HELP_TEXT)
+        case "add me once":
+            # upsert so users can trivially switch between participation levels without needed to delete themselves
+            conn.execute("INSERT OR REPLACE INTO users (user_id, participation) VALUES (?, ?)", (author_id, ParticipationLevel.ONCE.value))
+            print(f"Added user {author_id} once")
+            await message.channel.send(f"Okay, <@{author_id}>, I have added you for the next spin")
+        case "add me always":
+            conn.execute("INSERT OR REPLACE INTO users (user_id, participation) VALUES (?, ?)", (author_id, ParticipationLevel.ALWAYS.value))
+            print(f"Added user {author_id} always")
+            await message.channel.send(f"Okay, <@{author_id}>, I have added you to all future spins")
+        case "add me interested":
+            conn.execute("INSERT OR REPLACE INTO users (user_id, participation) VALUES (?, ?)", (author_id, ParticipationLevel.INTERESTED.value))
+            print(f"Added user {author_id} interested")
+            await message.channel.send(f"Okay, <@{author_id}>, I have marked you as interested")
+        case "remove me":
+            conn.execute("DELETE FROM users WHERE user_id = ?", (author_id,))
+            print(f"Deleted user {author_id}")
+            await message.channel.send(f"Okay, <@{author_id}>, I have removed you from all future spins")
+        case "prespin":
+            always = []
+            once = []
+            interested = []
+            cursor = conn.execute("SELECT user_id, participation FROM users")
+            for row in cursor.fetchall():
+                user_id = row[0]
+                participation = ParticipationLevel(row[1])
+                if participation == ParticipationLevel.ONCE:
+                    once.append(user_id)
+                elif participation == ParticipationLevel.ALWAYS:
+                    always.append(user_id)
+                else: #interested
+                    interested.append(user_id)
+            # maybe it'd be nice to sort the names alphabetically here but shrug
+            # everyone's going to get pinged anyway so ¯\_(ツ)_/¯
+            await message.channel.send(f"Always: {' '.join(['<@' + s + '>' for s in always])}")
+            await message.channel.send(f"Once: {' '.join(['<@' + s + '>' for s in once])}")
+            await message.channel.send(f"Interested: {' '.join(['<@' + s + '>' for s in interested])}")
+        case "spin":
+            # we don't actually need a filter on participation levels but we need the field
+            # we're just grabbing every user after all
+            # so we can distinguish how to act on the two types later
+            cursor = conn.execute("SELECT user_id, participation FROM users")
+            always = []
+            once = []
+            for row in cursor.fetchall():
+                user_id = row[0]
+                participation = ParticipationLevel(row[1])
+                if participation == ParticipationLevel.ONCE:
+                    once.append(user_id)
+                elif participation == ParticipationLevel.ALWAYS:
+                    always.append(user_id)
+                # interested users are irrelevant here, they didn't opt in for this round
+            all_participants = once + always
+            # don't let anyone get past here if there's no one in either list
+            if len(all_participants) == 0:
+                print("There are no participants, will not spin")
+                return
+            random.shuffle(all_participants)
+            
+            # we've shuffled everyone, we can now point everyone at the next person
+            msg = ""
+            for participant in all_participants:
+                msg += f"<@{participant}> -> "
+            # point the last participant back to the first to complete the circle
+            # we already added a trailing arrow so we can just append
+            # this probably gets extremely gross if there are a million people but shrug
+            msg += f"<@{all_participants[0]}>"
+            await message.channel.send(msg)
+
+            # okay, we're done, let's move the "once" participants back to "never"
+            for participant in once:
+                conn.execute("UPDATE users SET participation = ? WHERE user_id = ?", (ParticipationLevel.INTERESTED, author_id,))
+                print(f"Moving user {author_id} back to INTERESTED")
+        case _:
+            await message.channel.send(f"Sorry, <@{author_id}>, I don't understand. Have you tried asking for `help`?")
+    conn.commit()
+    conn.close()
+
+def maybe_init_db():
+    if not os.path.exists(DB_NAME):
+        print("No database found, initializing one...")
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        conn.execute('''CREATE TABLE users
+             (user_id TEXT PRIMARY KEY,
+              participation INTEGER);''')
+        conn.commit()
+        conn.close()
+        print("DB initialized")
+
+if __name__ == "__main__":
+    load_dotenv()
+    maybe_init_db()
+    client.run(os.getenv('BOT_TOKEN'))
